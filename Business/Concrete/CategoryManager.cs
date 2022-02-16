16291b56ce248d3374aa18386b08e78ce5b2d940
ziyacan yıldız <ziyacan.11@hotmail.com>
2022-02-16 21:29:51 +0300

    9.Ders

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _ıCategoryDal;
+        public CategoryManager(ICategoryDal ıCategoryDal)
+        {
+            _ıCategoryDal = ıCategoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            return _ıCategoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _ıCategoryDal.Get(p => p.CategoryId == categoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,14 +3,29 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
-namespace ConsoleUI
+namespace ConsoleUIp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //ProductTest(); 
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+
+
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAllByUnitPrice(50,120))
+            foreach (var product in productManager.GetAllByUnitPrice(50, 120))
             {
                 Console.WriteLine(product.ProductName);
             }
